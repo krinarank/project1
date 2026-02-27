523399c505f0fecc0110d7fdef12a3905a217574
krisha <chandgadhiyakrisha@gmail.com>
2026-02-28 01:58:19 +0530

    before pull mansi code 2.am

--- restaurant/deliverypanel/views.py
+++ restaurant/deliverypanel/views.py
@@ -41,6 +41,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
 from accounts.models import Customer
 from .models import DeliveryPerson
+from orders.models import *
 
 def delivery_login(request):
     if request.method == 'POST':
@@ -111,9 +112,11 @@ def delivery_dashboard(request):
 
     # 🔹 Active Orders (ACCEPTED)
     active_assignments = AssignOrder.objects.filter(
-        delivery_person=delivery,
-        status='ACCEPTED'
-    ).select_related('order', 'user')
+      delivery_person=delivery,
+      status__in=['ACCEPTED', 'DELIVERED']
+    ).select_related('order', 'user').prefetch_related(
+      'order__orderhaspayment_set__payment'
+    )
 
     # 🔹 Delivered Orders
     delivered_assignments = AssignOrder.objects.filter(
@@ -287,6 +290,27 @@ def verify_otp(request):
     return render(request, 'accounts/verify_otp.html', {
        'verify_url': 'verify_otp'
     })
+
+def delivery_mark_paid(request, order_id):
+    if 'delivery_id' not in request.session:
+        return redirect('/delivery/login/')
+
+    order = get_object_or_404(Order, id=order_id)
+
+    order_payment = OrderHasPayment.objects.filter(order=order).select_related('payment').first()
+
+    if order_payment:
+        payment = order_payment.payment
+
+        if payment.method == 'COD' and payment.status == 'PENDING':
+            payment.status = 'PAID'
+            payment.amount_paid = payment.remaining_amount
+            payment.remaining_amount = 0
+            payment.save()
+
+            messages.success(request, "COD Payment received successfully.")
+
+    return redirect('delivery_dashboard')
 
 def reset_password(request):
     if request.method == 'POST':
